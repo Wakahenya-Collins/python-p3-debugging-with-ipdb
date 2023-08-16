781f1daea604cfc4c2141611abbe5e26e4289edb
Collins Wakahenya <wakahenyacollins@gmail.com>
2023-08-16 17:21:55 +0300

    Done with assignment

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,20 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-import ipdb
+# import ipdb
 
+# def plus_two(num):
+#     num = 3
+#     num + 2
+#     # ipdb.set_trace()
+#     return num
 def plus_two(num):
-    num + 2
-    return num
+    result = num + 2  
+    return result     
+
+def test_plus_two():
+    result = plus_two(3)
+    assert result == 5  
+
+test_plus_two() 
+
+
